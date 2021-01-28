a2a894602465973a675b3aafbf7a49966ec914be
unknown <darylbargamento@remotodojo.com>
2021-01-28 18:08:06 +0800

    done 103 lt casual applicants

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    flexGrow: {
+      '0': 0,
+     DEFAULT: 1,
+     DEFAULT: 2,
+     '1': 1,
+    }
   },
   variants: {
     extend: {},
